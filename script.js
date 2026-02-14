6430a037ff6b06a1439a0fe970033e41e0c93575
nyo <nyamochirnatsagdorj2@gmail.com>
2026-02-14 19:10:30 +0800

    github

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Дахиад бодоод үздээ",
+    "Яг сайн бодоод үзэээээ",
+    "Өөөөө Гуйж байна шдээээээ",
+    "Плииииииис",
+    "За дахиад нэг удаа бодоод үзчих",
+    "Одоо NO дарвал гомдох байх аа",
+    "Аймар гуниглана :(",
+    "Бүр аймар",
+    "Заза асуухаа болилооооо.",
+    "За тоглосымааа, Yes Дээр нь дарчихаа❤️"
 ];
 
 let messageIndex = 0;
